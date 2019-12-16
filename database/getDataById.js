94e51274501333ca64fc603ced88c1f2c325c8c3
EmilDino <EmilLD123@hotmail.com>
2019-12-16 14:07:36 +0100

    init

--- database/getDataById.js
+++ database/getDataById.js
@@ -1,18 +1,9 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
-/** 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("IKEA-Trådfri");
-  var query = { _id: "1" };
-  dbo.collection("Pærer").find(query).toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    db.close();
-  });
-});
-*/
-module.exports.getDataById = async () => {
+
+
+
+module.exports.getDataById = async (id) => {
     return new Promise((resolve, reject) => {
         MongoClient.connect(url, async function (err, db) {
             if (err) {
@@ -20,7 +11,7 @@ module.exports.getDataById = async () => {
                 throw err;
             }
             var dbo = db.db("IKEA-Trådfri");
-            var query = {_id: "1" };
+            var query = {_id: id };
             dbo.collection("Pærer").find(query).toArray(async function (err, result) {
                 if (err) {
                     reject(err);
